a126d8e85c8824a90472571eeb9b8c6a07ae1012
EstebanChablay <esteban-edu08@hotmail.com>
2021-06-01 23:57:08 -0500

    Workshop load

---
+++ workshops/chablaye/unit1/WS05StructuredPrmming/StructuredPrograming/StructuredPrograming.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package structuredprograming;
+
+/**
+ *
+ * @author Esteban
+ */
+public class StructuredPrograming {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
