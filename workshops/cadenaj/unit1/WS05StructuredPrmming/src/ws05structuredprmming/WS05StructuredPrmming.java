b19f4580eb29dd76269473401a580b13b9d5afa3
JeremyCadena <jejocad2000@hotmail.com>
2021-06-02 20:57:08 -0500

    workshop folders

--- workshops/cadenaj/unit1/WS05StructuredPrmming/src/ws05structuredprmming/WS05StructuredPrmming.java
+++ workshops/cadenaj/unit1/WS05StructuredPrmming/src/ws05structuredprmming/WS05StructuredPrmming.java
@@ -3,26 +3,28 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ws05structuredprograming;
+package ws05structuredprogramming;
 
 import java.util.Scanner;
+
 /**
  *
- * @author DELL 5470
+ * @author jejoc
  */
-public class WS05structuredprograming {
+public class WS05StructuredProgramming {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    
+     public static void main(String[] args) {
         //add()
-        float addend1 = 0.0F;
+        float addend1;
         float addend2;
         float sum;
         
         //multiply()
-        float multiplicand;
+        float muliplicand;
         float multiplier;
         float product;
         
@@ -37,43 +39,43 @@ public class WS05structuredprograming {
         float quotient;
         
         Scanner keyboardinput = new Scanner(System.in);
-        System.out.println("WS 05 Structured <programing using java, by Richard Gualotuña");
-        System.out.println("=== ADITION ====");
-        System.out.println("Enter the addens ->");
+        System.out.println("WS05 Structured Programming using JAVA, by Jeremy Cadena");
+        System.out.println("=== ADITION ===");
+        System.out.println("Enter the addends ");
         System.out.print("Addend 1 -> ");
         addend1 = keyboardinput.nextFloat();
         System.out.print("Addend 2 -> ");
         addend2 = keyboardinput.nextFloat();
         
         sum = add(addend1, addend2);
-        System.out.println("the addition of" + addend1 + "+" + addend2 + "=" + sum);
+        System.out.println("The addition of " + addend1 + " + " + addend2 + " = " + sum);
         
-        System.out.println("=== DIVISION ====");
-        System.out.println("Enter the adividend and divisor ->");
+        System.out.println("=== DIVITION ===");
+        System.out.println("Enter de dividend an divisor ");
         System.out.print("Dividend -> ");
         dividend = keyboardinput.nextFloat();
         System.out.print("Divisor -> ");
         divisor = keyboardinput.nextFloat();
         
         quotient = divide(dividend, divisor);
-        System.out.println("the division of" + dividend + "+" + divisor + "=" + quotient);
+        System.out.println("The divition of " + dividend + " /2 " + divisor + " = " + quotient);
     }
     
-    public static float add(float addend1, float addend2){
-        return addend1+addend2;
- 
+    public static float add(float addend1,float addend2){
+        float sum;
+        sum = addend1 + addend2;
+        return sum;
     }
     
-    public static float multiply(float multiplicand, float multiplier){
+    public static float multiply(float multiplicand,float multiplier){
         float product;
         product = multiplicand * multiplier;
         return product;
     }
     
-    public static float divide(float dividend, float divisor){
+    public static float divide(float dividend,float divisor){
         float quotient;
-        quotient = dividend / divisor;
+        quotient = dividend/divisor;
         return quotient;
     }
-    
 }
