82e18099dde7165c585ce7af72369ff249a5037d
DarwinVProg <drvaldiviezo2@espe.edu.ec>
2021-06-09 16:28:39 -0500

    workshops

---
+++ workshops/valdiviezod/unit1/WS10ClassDiagramImplementation/ClassDiagramImplementation/src/main/java/ec/edu/espe/theoretical/model/B.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.theoretical.model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Darwin Valdiviezo NullPunters ESPE-DCCO
+ */
+class B {
+    private int a;
+    private int b;
+    ArrayList<E> es = new ArrayList<>();
+    
+    public C m1(F f){
+        return new C();
+    }
+}
