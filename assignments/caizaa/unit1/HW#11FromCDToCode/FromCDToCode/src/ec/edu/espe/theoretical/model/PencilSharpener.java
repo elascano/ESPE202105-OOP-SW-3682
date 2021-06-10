1930f48b5d4759462f9bc7e4c510a28c17256de5
Alissoncaiza <alcaiza3@espe.edu.ec>
2021-06-09 23:32:46 -0500

    assignments

---
+++ assignments/caizaa/unit1/HW#11FromCDToCode/FromCDToCode/src/ec/edu/espe/theoretical/model/PencilSharpener.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.theoretical.model;
+
+/**
+ *
+ * @author Alisson Caiza ASUS ESPE-DCCO
+ */
+public class PencilSharpener {
+    private String material;
+    private int brand;
+    private String quality;
+    
+    public void sell(){
+        
+    }
+    public void offer(){
+        
+    }
+    public void buy(){
+    
+    }
+    
+}
