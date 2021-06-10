99500d893da00a82086a81a7a9573c60c2758ba0
DarwinVProg <drvaldiviezo2@espe.edu.ec>
2021-06-09 21:36:53 -0500

    assigments

---
+++ assignments/valdiviezod/unit1/HW11CDToCode/ApplianceStoreSimulator/src/main/java/ec/edu/espe/appliancestore/model/Iron.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.appliancestore.model;
+
+/**
+ *
+ * @author Darwin Valdiviezo NullPunters ESPE-DCCO
+ */
+class Iron {
+    private float size;
+    private int price;
+    private String model;
+    private String material;
+    private int power;
+    private int serialnumber;
+    
+    public void buy(){
+        
+    }
+    public void sell(){
+        
+    }
+    public void instock(){
+        
+    }
+    public Gain generateprofit(){
+        return new Gain();
+    }
+   
+}
