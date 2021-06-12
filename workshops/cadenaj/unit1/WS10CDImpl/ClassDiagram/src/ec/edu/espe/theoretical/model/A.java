3777fd8dfd1dbf4545376bcd713108a62e13638d
JeremyCadena <jejocad2000@hotmail.com>
2021-06-11 21:06:48 -0500

    WS11 added

--- workshops/cadenaj/unit1/WS10CDImpl/ClassDiagram/src/ec/edu/espe/theoretical/model/A.java
+++ workshops/cadenaj/unit1/WS10CDImpl/ClassDiagram/src/ec/edu/espe/theoretical/model/A.java
@@ -14,12 +14,59 @@ import java.util.ArrayList;
 public class A {
     private B b;
     private ArrayList<C> cs = new ArrayList<>();
+    private int i;
+    
     
     public C m(D d){
+        System.out.println("Inside method A.m(D d), receiving parameter d -> "
+                + d + "and returning a C objetc");
         return new C();
     }
     
     public D m(){
+        System.out.println("Inside method A.m() that returns a D object");
         return new D();
+    }
+
+     /**
+     * @return the i
+     */
+    public int getI() {
+        return i;
+    }
+    
+    /**
+     * @return the b
+     */
+    public B getB() {
+        return b;
+    }
+
+    /**
+     * @param i the b to set
+     */
+    public void setI(int i) {
+        this.i = i;
+    }
+    
+    /**
+     * @param b the b to set
+     */
+    public void setB(B b) {
+        this.b = b;
+    }
+
+    /**
+     * @return the cs
+     */
+    public ArrayList<C> getCs() {
+        return cs;
+    }
+
+    /**
+     * @param cs the cs to set
+     */
+    public void setCs(ArrayList<C> cs) {
+        this.cs = cs;
     }
 }
