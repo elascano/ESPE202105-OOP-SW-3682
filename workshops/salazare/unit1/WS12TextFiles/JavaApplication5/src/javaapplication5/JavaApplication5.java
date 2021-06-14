d8b78a3fd661da84a479550a4c1e2569cd8dc1d0
SalazarMatthew <emsalazar1@espe.edu.ec>
2021-06-14 18:58:47 -0500

    workshops

---
+++ workshops/salazare/unit1/WS12TextFiles/JavaApplication5/src/javaapplication5/JavaApplication5.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication5;
+
+
+import.java.io.IOException;
+import java.io.FileWriter;
+
+/**
+ *
+ * @author Salazar Matthew NullPointers ESPE-DCC
+ */
+public class JavaApplication5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        
+        FileWriter fichero = new FileWriter("C:\Matthew 2021\PROGRAMACION ORIENTADA\salazare\ESPE202105-OOP-SW-3682\workshops\salazare\unit1\WS12TextFiles\person.txt");
+    }
+    
+}
