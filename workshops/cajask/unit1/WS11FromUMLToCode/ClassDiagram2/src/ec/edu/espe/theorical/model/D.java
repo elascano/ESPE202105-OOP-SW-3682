07d3f68dffe7a3b7a5c5697373946f4be6400613
KarlaCajas <klcajas1@espe.edu.ec>
2021-06-14 00:24:55 -0500

    WORKSHOP

---
+++ workshops/cajask/unit1/WS11FromUMLToCode/ClassDiagram2/src/ec/edu/espe/theorical/model/D.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.theorical.model;
+
+/**
+ *
+ * @author Lizbeth Cajas EMCL. Java ESPE-DCCO
+ */
+public class D {
+    private boolean b;
+
+    public D(boolean b) {
+        this.b = b;
+    }
+
+    public D() {
+    }
+    
+    
+    
+    public B m(){
+        System.out.println("inside D.m() method that returns a B object");
+        return new B();
+    }
+    
+    public int m(F f){
+        System.out.println("inside D.m() method that receives f -> " +f);
+        return 0;
+    }
+
+    public boolean isB() {
+        return b;
+    }
+
+    public void setB(boolean b) {
+        this.b = b;
+    }
+}
