a84d0b978e95dd1a58f717510608e930ac0bc3c6
Ariel González <aagonzalez8@espe.edu.ec>
2021-06-13 21:03:11 -0500

    HW12

---
+++ assignments/chamorrom/unit1/HW11FromCDToCode/Sanamed Account System/src/ec/edu/espe/SanamedAccountSystem/model/Product.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.SanamedAccountSystem.model;
+
+/**
+ *
+ * @author Myckel Chamorro EMCL. Java ESPE-DCCO
+ */
+public class Product {
+   private Medicins medicine;
+   private Cosmetic cosmetic;
+   private Others other;
+   private int quantity;
+   private float individualValue;
+   
+   public Medicins addMedicine(Medicins medicine){
+       return new Medicins();
+   }
+   
+   public Cosmetic addCosmetic(Cosmetic cosmetic){
+       return new Cosmetic();
+   }
+   
+   public Others addOther(Others other){
+       return new Others();
+   }
+   
+}
