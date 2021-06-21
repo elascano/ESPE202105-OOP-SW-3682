da9f4d9ceaefdd23c8462ad78ced0c57fc330f63
DiegoAlejandroPortilla <daportilla1@espe.edu.ec>
2021-06-20 22:32:28 -0500

    HW18

---
+++ assignments/portillad/unit1/HW18JSON/ApplianceStoreSimulator/src/ec/edu/espe/appliancestore/model/Computer.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.appliancestore.model;
+
+/**
+ *
+ * @author Diego Portilla NullPointers ESPE-DCCO
+ */
+public class Computer {
+    private int power;
+    private String material;
+    private float price;
+    private String storage;
+    private int serialnumber;
+        
+    public void buy(){
+        
+    }
+    public void sell(){
+        
+    }
+    public void tostock(){
+        
+    }
+    public Gain generateprofit(){
+    return new Gain();
+    
+    }
+}
