0460b3ba792c5426703c6a5cd2a89ba276411fca
Ariel González <aagonzalez8@espe.edu.ec>
2021-06-21 04:34:50 -0500

    WS14

---
+++ workshops/gonzaleza/unit1/WS14Json/ChickenFarmSimulator/src/ec/edu/espe/farm/model/Chicken.java
@@ -0,0 +1,143 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.farm.model;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+/**
+ *
+ * @author Ariel Gonzalez InnovaCode ESPE-DCCO
+ */
+public class Chicken {
+    private int id;
+    private String name;
+    private String color;
+    private LocalDate currentDate;
+    private LocalDate bornOn;
+    private int age;
+    private boolean isMolting;
+
+    public Chicken(int id, String name, String color, LocalDate currentDate, LocalDate bornOn, int age, boolean isMolting) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.currentDate = currentDate;
+        this.bornOn = bornOn;
+        this.age = age;
+        this.isMolting = isMolting;
+    }
+
+    public Chicken() {
+    }
+
+    @Override
+    public String toString() {
+        return "Chicken{" + "id=" + id + ", name=" + name + ", color=" + color + ", age=" + age + ", isMolting=" + isMolting + '}';
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the color
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * @param color the color to set
+     */
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    /**
+     * @return the currentDate
+     */
+    public LocalDate getCurrentDate() {
+        return currentDate;
+    }
+
+    /**
+     * @param currentDate the currentDate to set
+     */
+    public void setCurrentDate(LocalDate currentDate) {
+        this.currentDate = currentDate;
+    }
+
+    /**
+     * @return the bornOn
+     */
+    public LocalDate getBornOn() {
+        return bornOn;
+    }
+
+    /**
+     * @param bornOn the bornOn to set
+     */
+    public void setBornOn(LocalDate bornOn) {
+        this.bornOn = bornOn;
+    }
+
+    /**
+     * @param currentDate
+     * @param bornOn
+     * @return the age
+     */
+    public int getAge(LocalDate currentDate, LocalDate bornOn) {
+        age = Period.between(bornOn, currentDate).getYears();
+        return age;
+    } 
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * @return the isMolting
+     */
+    public boolean isIsMolting() {
+        return isMolting;
+    }
+
+    /**
+     * @param isMolting the isMolting to set
+     */
+    public void setIsMolting(boolean isMolting) {
+        this.isMolting = isMolting;
+    }
+
+}
