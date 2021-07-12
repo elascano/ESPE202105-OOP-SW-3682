dfb7e77ca92b079fa119dacbbb3ca284e277ab19
EscobarIsaac <iescoba@espe.edu.ec>
2021-07-12 09:37:26 -0500

    abstract

---
+++ workshops/escobari/unit2/WS23AbstractClass/AbstractClass/src/ec/edu/espe/abstractclass/view/AbstractClass.java
@@ -0,0 +1,14 @@
+
+package ec.edu.espe.abstractclass.view;
+
+/**
+ *
+ * @author Myckel Chamorro EMCL.java ESPE-DCCO
+ */
+public class AbstractClass {
+    public static void main(String[] args) {
+        System.out.println("Abstract Class");
+       
+    }
+    
+}
