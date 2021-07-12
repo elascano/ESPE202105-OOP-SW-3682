61e7708b03f90d23218bf954c98912945a975efe
elascano <edison_lascano@yahoo.com>
2021-07-12 07:24:16 -0500

    inheritance, polymorphism, abstraction diagram

---
+++ workshops/elascano/unit1/WS09FromUmlToCode/ClassDiagram/src/ec/edu/espe/theoretical/model/F.java
@@ -0,0 +1,27 @@
+/** Copyright ESPE-DECC
+*/
+
+package ec.edu.espe.theoretical.model;
+
+/**
+ *
+ * @author Edison Lascano Hypertech ESPE-DCCO
+ */
+public class F {
+    private double d;
+
+    /**
+     * @return the d
+     */
+    public double getD() {
+        return d;
+    }
+
+    /**
+     * @param d the d to set
+     */
+    public void setD(double d) {
+        this.d = d;
+    }
+
+}
