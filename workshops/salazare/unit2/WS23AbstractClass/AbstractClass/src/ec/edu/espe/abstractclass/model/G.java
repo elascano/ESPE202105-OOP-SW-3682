18d1c857f77d4a1e4dc2d0d618dfac5b02fbf420
SalazarMatthew <emsalazar1@espe.edu.ec>
2021-07-12 09:39:09 -0500

    AbstrctClass

---
+++ workshops/salazare/unit2/WS23AbstractClass/AbstractClass/src/ec/edu/espe/abstractclass/model/G.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.abstractclass.model;
+
+/**
+ *
+ * @author Salazar Matthew NullPointers ESPE-DCC
+ */
+public class G {
+    private int z;
+
+    /**
+     * @return the z
+     */
+    public int getZ() {
+        return z;
+    }
+
+    /**
+     * @param z the z to set
+     */
+    public void setZ(int z) {
+        this.z = z;
+    }
+    
+}
