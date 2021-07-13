54ecc786b4665d724f2af8bfe3834999f429a9a9
DarwinVProg <drvaldiviezo2@espe.edu.ec>
2021-07-12 21:19:12 -0500

    workshop

---
+++ workshops/valdiviezod/unit2/WS23AbstractClass/AbstractClasses/src/ec/edu/espe/abstractclasses/model/F.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.abstractclasses.model;
+
+/**
+ *
+ * @author Nicolas Suquillo NullPointers ESPE-DCCO
+ * @author Darwin Valdiviezo NullPointers ESPE-DCCO
+ */
+public class F {
+    
+    private String r;
+
+    @Override
+    public String toString() {
+        return "F{" + "r=" + r + '}';
+    }
+
+    public F(String r) {
+        this.r = r;
+    }
+
+    public F(){
+        r="";
+    }
+    /**
+     * @return the r
+     */
+    public String getR() {
+        return r;
+    }
+
+    /**
+     * @param r the r to set
+     */
+    public void setR(String r) {
+        this.r = r;
+    }
+
+}
