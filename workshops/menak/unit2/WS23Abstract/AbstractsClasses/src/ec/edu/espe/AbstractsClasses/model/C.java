a13ef2810cdcdb48a913f4541acc765fe8bb2433
MenaPaul <kpmena1@espe.edu.ec>
2021-07-13 23:54:29 -0500

    Workshops

---
+++ workshops/menak/unit2/WS23Abstract/AbstractsClasses/src/ec/edu/espe/AbstractsClasses/model/C.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.AbstractsClasses.model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author pc
+ */
+public class C {
+  ArrayList<H>h= new ArrayList<>();  
+  public static void m(F f){
+  }
+    public static void m(float f){
+  }
+}
+
