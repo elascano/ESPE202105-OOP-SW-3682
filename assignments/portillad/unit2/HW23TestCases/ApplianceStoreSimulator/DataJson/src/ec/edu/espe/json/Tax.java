33b4628799a0981b209461614438da95912a4ddb
DiegoAlejandroPortilla <daportilla1@espe.edu.ec>
2021-07-20 19:47:55 -0500

    HW23

--- assignments/portillad/unit2/HW23TestCases/ApplianceStoreSimulator/DataJson/src/ec/edu/espe/json/Tax.java
+++ assignments/portillad/unit2/HW23TestCases/ApplianceStoreSimulator/DataJson/src/ec/edu/espe/json/Tax.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package utils;
+package ec.edu.espe.json;
 
 /**
  *
@@ -22,6 +22,5 @@ public class Tax {
         totalPrice =price +computeIva(price);
         return totalPrice;
     }
-    //TEST CASES 
-    //input/s      expected
+
 }
