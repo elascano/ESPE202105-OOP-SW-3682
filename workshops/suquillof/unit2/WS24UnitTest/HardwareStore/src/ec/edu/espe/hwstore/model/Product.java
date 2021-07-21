bce099584e3c8396650a32c7862a288aaebc8b19
NicolasSuquillo <nicosuquillo2001@gmail.com>
2021-07-21 00:17:55 -0500

    WS24

---
+++ workshops/suquillof/unit2/WS24UnitTest/HardwareStore/src/ec/edu/espe/hwstore/model/Product.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.hwstore.model;
+
+/**
+ *
+ * @author Nicolas Suquillo NullPointers ESPE-DCCO
+ */
+public class Product {
+ 
+    private int id;
+    private float price;
+    private float salePrice;
+    private float totalPrice;
+    private String description;
+
+    public Product(int id, float price, String description) {
+        this.id = id;
+        this.price = price;
+        this.description = description;
+    }
+
+    
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the price
+     */
+    public float getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    /**
+     * @return the salePrice
+     */
+    public float getSalePrice() {
+        return salePrice;
+    }
+
+    /**
+     * @param salePrice the salePrice to set
+     */
+    public void setSalePrice(float salePrice) {
+        this.salePrice = salePrice;
+    }
+
+    /**
+     * @return the totalPrice
+     */
+    public float getTotalPrice() {
+        return totalPrice;
+    }
+
+    /**
+     * @param totalPrice the totalPrice to set
+     */
+    public void setTotalPrice(float totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    
+}
