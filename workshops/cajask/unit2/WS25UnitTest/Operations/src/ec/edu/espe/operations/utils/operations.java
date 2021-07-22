af778cb66980871c3d03b814bc9efd0eefdddbe5
KarlaCajas <klcajas1@espe.edu.ec>
2021-07-21 23:25:59 -0500

    WORKSHOP

---
+++ workshops/cajask/unit2/WS25UnitTest/Operations/src/ec/edu/espe/operations/utils/operations.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.operations.utils;
+
+/**
+ *
+ * @author Lizbeth Cajas
+ */
+public class operations {
+    
+    public static float addition(float NUM1, float NUM2) {
+        float Sumtotal;
+        Sumtotal = NUM1 + NUM2;
+        return Sumtotal;
+    }
+
+    public static float subtract(float minuend, float subtrahend) {
+        float difference;
+        difference = minuend - subtrahend;
+        return difference;
+    }
+    
+}
