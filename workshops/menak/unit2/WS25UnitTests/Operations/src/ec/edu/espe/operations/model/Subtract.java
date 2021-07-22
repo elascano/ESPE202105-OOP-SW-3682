2ed88ad6948152b9a2614c75e4a9feff208a3f7b
MenaPaul <kpmena1@espe.edu.ec>
2021-07-21 20:40:23 -0500

    Workshops

---
+++ workshops/menak/unit2/WS25UnitTests/Operations/src/ec/edu/espe/operations/model/Subtract.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.operations.model;
+
+/**
+ *
+ * @author Paul Mena The Programmers ESPE-DCCO
+ */
+public class Subtract {
+    
+    
+    public static float computeSubtract(float minuend, float subtraend){
+        
+        float resultDifference;
+    
+        resultDifference =  minuend - subtraend ;
+        return resultDifference;
+    
+    }
+    
+}
