be6a472500bb0ccee9afd7627c405bb910776f29
Eduardo Antonio Mortensen Franco <84451615+EDUARDOMORTENSEN@users.noreply.github.com>
2021-07-29 22:13:40 -0500

    WS

---
+++ workshops/mortensene/unit2/WS26GUI/GroseryStoreModel/GroseryStoreModel/src/ec/edu/espe/groseryStoreModel/model/Appetizers.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.groseryStoreModel.model;
+
+/**
+ *
+ * @author Paul Mena The Programmers ESPE-DCCO
+ */
+public class Appetizers extends Inventory{
+    
+    
+    private String type;
+    private String brand;
+    private int id;
+    private float price;
+    private float weight;
+
+     public Appetizers(float weight,int id, float price, String type, String brand) {
+        super(id, price, type, brand);
+        this.weight=weight;
+    }
+@Override
+ public String toString() {
+        return "Appetizers{" + super.toString()+"weight=" + weight + '}';
+    }
+
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * @return the brand
+     */
+    public String getBrand() {
+        return brand;
+    }
+
+    /**
+     * @param brand the brand to set
+     */
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the price
+     */
+    public float getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    /**
+     * @return the weight
+     */
+    public float getWeight() {
+        return weight;
+    }
+
+    /**
+     * @param weight the weight to set
+     */
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+
+
+}
