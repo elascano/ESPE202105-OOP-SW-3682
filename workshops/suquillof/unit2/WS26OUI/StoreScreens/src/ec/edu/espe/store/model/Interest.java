70cf7f87443a0137506a6b3e93d7d4ad55059288
NicolasSuquillo <nicosuquillo2001@gmail.com>
2021-07-31 00:43:19 -0500

    WS26

---
+++ workshops/suquillof/unit2/WS26OUI/StoreScreens/src/ec/edu/espe/store/model/Interest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.store.model;
+
+/**
+ *
+ * @author Nicolas Suquillo NullPointers ESPE-DCCO
+ */
+public class Interest {
+    
+    private String name;
+    private String description;
+
+    public Interest(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+}
