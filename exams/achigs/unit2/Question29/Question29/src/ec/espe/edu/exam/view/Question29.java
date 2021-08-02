2547a8d10f96eba5be4e8e1ee247cb5fee19c5ce
StevenJossueAchig <stevenjossue5@gmail.com>
2021-08-02 09:34:41 -0500

    Exam Unit 2

---
+++ exams/achigs/unit2/Question29/Question29/src/ec/espe/edu/exam/view/Question29.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.espe.edu.exam.view;
+
+/**
+ *
+ * @author Steven Achig Future Programmers ESPE-DCCO
+ */
+public class Question29 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
