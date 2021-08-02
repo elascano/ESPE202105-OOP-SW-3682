afd6704234bbfffb8a848e8d9ea776ba0211b554
EstebanChablay <esteban-edu08@hotmail.com>
2021-08-02 09:58:32 -0500

    HW

---
+++ assignments/chablaye/unit2/HW26MongoDB/ClienteSanamed/src/ec/edu/espe/cliente/model/Client.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.cliente.model;
+
+/**
+ *
+ * @author Esteban Chablay EMCL. Java ESPE-DCCO
+ */
+public class Client {
+    private String id;
+    private String name;
+    private int age;
+
+    public Client() {
+    }
+
+    public Client(String id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" + "id=" + id + ", name=" + name + ", age=" + age + '}';
+    }
+
+}
