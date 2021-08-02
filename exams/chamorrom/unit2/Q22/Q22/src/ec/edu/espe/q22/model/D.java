72b62fa98734f930c33356984d41f23b2c646d48
MyckelChamorro <mschamorro@espe.edu.ec>
2021-08-02 09:51:41 -0500

    EXAM

---
+++ exams/chamorrom/unit2/Q22/Q22/src/ec/edu/espe/q22/model/D.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.q22.model;
+
+/**
+ *
+ * @author Myckel Chamorro EMCL.java ESPE-DCCO
+ */
+public class D extends A {
+
+    private E[] es = new E[4];
+
+    public D(int a, E[] es, A aa) {
+        super(a,aa);
+        this.es = es;
+
+    }
+    /**
+     * @return the es
+     */
+    public E[] getEs() {
+        return es;
+    }
+
+    /**
+     * @param es the es to set
+     */
+    public void setEs(E[] es) {
+        this.es = es;
+    }
+
+}
