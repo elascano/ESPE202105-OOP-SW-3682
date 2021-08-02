913ad439d0bd7ece90457d9fcd9d511cb78eba71
Asmalkevin1999 <kdasmal@espe.edu.ec>
2021-08-02 13:37:11 -0500

    ws25

---
+++ workshops/asmalk/unit2/WS25UnitTest/mathematicsOperation/src/ec/espe/edu/mathemathics/view/MathematicsOperation.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.espe.edu.mathemathics.view;
+
+/**
+ *
+ * @author Asmal Kevin
+ */
+public class MathematicsOperation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    }
+    
+}
