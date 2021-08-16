d8b742bb0c9924561cbaf6fa9cdd4c27b399febd
SalazarMatthew <emsalazar1@espe.edu.ec>
2021-08-15 22:12:54 -0500

    JTable

---
+++ assignments/salazare/unit3/HW27JTable/Menu/src/ec/edu/espe/store/model/Fridge.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.store.model;
+
+/**
+ *
+ * @author Diego Portilla NullPointers ESPE-DCCO
+ */
+public class Fridge extends ApplianceStore {
+    
+    private float size;
+    private String model;
+    private String Brand;
+
+    public Fridge(float size, String model, String Brand, int serialNumber, float price) {
+        super(serialNumber, price);
+        this.size = size;
+        this.model = model;
+        this.Brand = Brand;
+    }
+
+    /**
+     * @return the size
+     */
+    public float getSize() {
+        return size;
+    }
+
+    /**
+     * @param size the size to set
+     */
+    public void setSize(float size) {
+        this.size = size;
+    }
+
+    /**
+     * @return the model
+     */
+    public String getModel() {
+        return model;
+    }
+
+    /**
+     * @param model the model to set
+     */
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    /**
+     * @return the Brand
+     */
+    public String getBrand() {
+        return Brand;
+    }
+
+    /**
+     * @param Brand the Brand to set
+     */
+    public void setBrand(String Brand) {
+        this.Brand = Brand;
+    }
+    
+    
+    
+    
+}
---
+++ assignments/salazare/unit3/HW27JTable/Menu/src/ec/edu/espe/store/model/Microwave.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.store.model;
+
+
+/**
+ *
+ * @author Diego Portilla NullPointers ESPE-DCCO
+ */
+public class Microwave extends ApplianceStore {
+   
+    private float size;
+    private float weight;
+    private String material;
+    private String model;
+
+    public Microwave( int serialNumber, float price,float size, float weight, String material, String model) {
+        super(serialNumber, price);
+        this.size = size;
+        this.weight = weight;
+        this.material = material;
+        this.model = model;
+    }
+
+      
+
+    @Override
+    public String toString() {
+        return "Microwave{" + super.toString() + ", size=" + size + " Cm" + ", weight=" + weight + "Kg" + ", material=" + material + ", model=" + model + '}';
+    }
+        
+             
+   
+
+    /**
+     * @return the size
+     */
+    public float getSize() {
+        return size;
+    }
+
+    /**
+     * @param size the size to set
+     */
+    public void setSize(float size) {
+        this.size = size;
+    }
+
+    /**
+     * @return the weight
+     */
+    public float getWeight() {
+        return weight;
+    }
+
+    /**
+     * @param weight the weight to set
+     */
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    /**
+     * @return the material
+     */
+    public String getMaterial() {
+        return material;
+    }
+
+    /**
+     * @param material the material to set
+     */
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    
+    /**
+     * @return the model
+     */
+    public String getModel() {
+        return model;
+    }
+
+    /**
+     * @param model the model to set
+     */
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+}
