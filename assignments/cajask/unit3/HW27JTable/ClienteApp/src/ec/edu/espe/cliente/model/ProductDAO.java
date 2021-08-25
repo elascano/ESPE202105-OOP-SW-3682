190a59f550f958b857afc62925f6e5bd3e6fff4e
KarlaCajas <klcajas1@espe.edu.ec>
2021-08-24 22:34:52 -0500

    HMW

---
+++ assignments/cajask/unit3/HW27JTable/ClienteApp/src/ec/edu/espe/cliente/model/ProductDAO.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.cliente.model;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCursor;
+import ec.edu.espe.cliente.view.ProductFrm;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Esteban Chablay EMCL. Java ESPE-DCCO
+ */
+public class ProductDAO {
+    public void insertProduct(Product p){
+        ConexionP con = new ConexionP();
+        BasicDBObject documento = new BasicDBObject();
+        documento.put("type","'" +p.getType()+ "'");
+        documento.put("name","'" +p.getName()+ "'");
+        documento.put("id","'" +p.getId()+ "'");
+        documento.put("description","'" +p.getDescription()+ "'");
+        documento.put("price","'" +p.getPrice()+ "'");
+        con.coleccion.insert(documento);    
+    }
+    
+    public void  mostrarColeccion(ProductFrm vista){
+        ConexionP objCon = new ConexionP();
+        DBCursor cursor = objCon.coleccion.find();
+        Client client = new Client();
+        
+        Object [] objeto = new Object[3];
+        
+        
+        while(cursor.hasNext()){
+            System.out.println(cursor.next());
+            vista.txtFieldDatos.setText((String) vista.txtFieldDatos.getText()+ "\n" +cursor.next().get("name"));
+        }    
+    }
+}
