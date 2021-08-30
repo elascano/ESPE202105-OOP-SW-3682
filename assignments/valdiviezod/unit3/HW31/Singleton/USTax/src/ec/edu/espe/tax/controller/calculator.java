e560dc62bb1fff360d29fc8438abf0dbbb2dfe0d
DarwinVProg <drvaldiviezo2@espe.edu.ec>
2021-08-29 19:57:35 -0500

    assigments

---
+++ assignments/valdiviezod/unit3/HW31/Singleton/USTax/src/ec/edu/espe/tax/controller/calculator.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.tax.controller;
+
+import ec.edu.espe.tax.model.USTax;
+
+/**
+ *
+ * @author Darwin Valdiviezo NullPointers ESPE-DCCO
+ */
+public class calculator {
+    
+}
