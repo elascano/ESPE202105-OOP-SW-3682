99c8fac7394ccf743006713550ebb5ae23201ed8
CristianDavidFelix <david.df.01@hotmail.com>
2021-08-31 06:33:33 -0500

    Added Patterns

---
+++ assignments/felixc/unit3/HW33AbstractFactory/GUIFactory/src/ec/espe/edu/guifactory/model/GUIFactory.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.espe.edu.guifactory.model;
+
+/**
+ *
+ * @author Cristian Félix InnovaCode ESPE-DCCO
+ */
+public abstract class GUIFactory {
+    public static GUIFactory getFactory(){
+        int sys = readFromConfigFile("OS_TYPE");
+        
+        if(sys == 0){
+            return new WindowsFactory();
+        } else if(sys == 1){
+            return new LinuxFactory();
+        } else 
+            return new MacOSFactory();
+    }
+
+    private static int readFromConfigFile(String os_type) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public abstract Button createButton();
+    public abstract Menu createMenu();
+}
