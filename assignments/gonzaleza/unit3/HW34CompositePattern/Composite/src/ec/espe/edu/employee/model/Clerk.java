1b638fafde7a56696db1721f8012094632ef33b9
Ariel González <aagonzalez8@espe.edu.ec>
2021-08-31 00:13:36 -0500

    HW

---
+++ assignments/gonzaleza/unit3/HW34CompositePattern/Composite/src/ec/espe/edu/employee/model/Clerk.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.espe.edu.employee.model;
+
+/**
+ *
+ * @author Ariel Gonzalez InnovaCode ESPE-DCCO
+ */
+public class Clerk extends Employee {
+    
+    public Clerk(String aName){
+        this();
+        name = aName;
+    }
+    
+    public void stateName(){
+        super.stateName();
+    }
+    
+    public Clerk(){
+        title = "Clerk";
+    }
+}
