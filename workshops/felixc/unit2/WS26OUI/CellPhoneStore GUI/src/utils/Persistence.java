6a83ccd4266d7473b46ede9b80dfb8c949458e7c
Eduardo Antonio Mortensen Franco <84451615+EDUARDOMORTENSEN@users.noreply.github.com>
2021-09-01 00:27:02 -0500

    HW

---
+++ workshops/felixc/unit2/WS26OUI/CellPhoneStore GUI/src/utils/Persistence.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+/**
+ *
+ * @author 59399
+ */
+public interface Persistence {
+    public void crete(String data , String table);
+    public String read(String table);
+    public String find(String seachString ,String table);
+    public boolean update(String seachString , String data , String table);
+    public boolean delete(String seacString , String table );
+    
+}
