8af89208cea898fb446f062a4e70764834db3e87
CristianDavidFelix <david.df.01@hotmail.com>
2021-09-02 07:04:35 -0500

    Update Sort App

---
+++ assignments/felixc/unit3/HW35SortApp/SortApp/src/ec/espe/edu/sortapp/controller/InsertionSort.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.espe.edu.sortapp.controller;
+
+/**
+ *
+ * @author Cristian Félix InnovaCode ESPE-DCCO
+ */
+public class InsertionSort implements SortingStrategy {
+
+    @Override
+    public int[] sort(int[] data) {
+        int aux;
+        int counter1;
+        int counter2;
+        for (counter1 = 1; counter1 < data.length; counter1++) {
+            aux = data[counter1];
+            for (counter2 = counter1 - 1; counter2 >= 0 && data[counter2] > aux; counter2--) {
+                data[counter2 + 1] = data[counter2];
+                data[counter2] = aux;
+            }
+        }
+        return data;
+    }
+    
+}
