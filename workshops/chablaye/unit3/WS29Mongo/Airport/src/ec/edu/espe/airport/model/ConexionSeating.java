c2fbbddf3069e8c99e78f9857c76fa2e4c50edc5
EstebanChablay <esteban-edu08@hotmail.com>
2021-09-03 18:06:44 -0500

    WS

---
+++ workshops/chablaye/unit3/WS29Mongo/Airport/src/ec/edu/espe/airport/model/ConexionSeating.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.airport.model;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+
+/**
+ *
+ * @author Esteban Chablay EMCL. Java ESPE-DCCO
+ */
+public class ConexionSeating {
+    DB baseDatos;
+    DBCollection coleccion;
+    BasicDBObject documento = new BasicDBObject();
+    MongoClientURI uri = new MongoClientURI("mongodb+srv://Esteban:pooEspe123@cluster0.seaw1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+    
+    
+    public ConexionSeating()
+    {
+        try{
+            MongoClient mongo = new MongoClient(uri);
+            baseDatos = mongo.getDB("airport");
+            coleccion = baseDatos.getCollection("number of seats");
+            System.out.println("Conexion a base de datos exitoso.");
+        }
+        catch(Exception e)
+        {
+            
+        }
+    }
+}
