0b845aaef9766d915e1e317297e582c52e12cfda
Alissoncaiza <alcaiza3@espe.edu.ec>
2021-09-03 18:08:05 -0500

    assignments

---
+++ assignments/caizaa/unit3/HW#33AbstractFactory/ClientApp/src/ec/edu/espe/gui/model/MacFactory.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.gui.model;
+
+import ec.edu.espe.gui.controller.MacButton;
+import ec.edu.espe.gui.controller.MacMenu;
+import ec.edu.espe.gui.model.Button;
+import ec.edu.espe.gui.model.GUIFactory;
+import ec.edu.espe.gui.model.Menu;
+
+/**
+ *
+ * @author Alisson Caiza ASUS ESPE-DCCO
+ */
+public class MacFactory extends GUIFactory{
+
+    public Button createButton() {
+        return (new MacButton());
+    }
+
+    public Menu createMenu() {
+        return (new MacMenu());
+    }
+}
