5ef1127fd4ff43fbfa1fe82b33b57c3cb68fad76
EstebanChablay <esteban-edu08@hotmail.com>
2021-09-03 17:04:58 -0500

    HW

---
+++ assignments/chablaye/unit3/HW33AbstractFactory/AbstractFactory/src/ec/edu/espe/model/abstractf/WinFact.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.model.abstractf;
+
+/**
+ *
+ * @author Esteban Chablay EMCL. Java ESPE-DCCO
+ */
+public class WinFact implements GUIFactory{
+
+    @Override
+    public void getFactory() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Button createButton() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Menu createMenu() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+    
+}
