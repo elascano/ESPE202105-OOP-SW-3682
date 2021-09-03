a3673f2f66cf9d1adbae4c14f033199d6570bcd6
DarwinVProg <drvaldiviezo2@espe.edu.ec>
2021-09-03 16:35:25 -0500

    assigments

---
+++ assignments/valdiviezod/unit3/HW33/AbstractFactory/GUIFactory/src/ec/edu/espe/factory/model/WinButton.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.factory.model;
+
+/**
+ *
+ * @author Darwin Valdiviezo NullPointers ESPE-DCCO
+ */
+public class WinButton extends Button{
+
+    @Override
+    public void paint() {
+        System.out.println("I'm a WinButton: " + caption);
+        
+    }
+    
+}
