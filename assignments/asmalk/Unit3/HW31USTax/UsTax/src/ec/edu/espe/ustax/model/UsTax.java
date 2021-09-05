e452a6ef50ae4cd4b0b03923f2a3ea85f42728de
Asmalkevin1999 <kdasmal@espe.edu.ec>
2021-09-05 13:31:15 -0500

    ws

---
+++ assignments/asmalk/Unit3/HW31USTax/UsTax/src/ec/edu/espe/ustax/model/UsTax.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.ustax.model;
+
+/**
+ *
+ * @author Asmal Kevin
+ */
+
+public class UsTax {
+    private static UsTax instace;
+    private float taxPercenage;
+    
+    private UsTax() {
+        
+    }
+     public static UsTax getInstance (){
+        if(instace == null){
+            instace = new UsTax();
+        }
+            return instace;
+        
+        
+        
+    }
+     
+     
+    public float salesTotal(float amount){
+        float ivaValue ;
+        float taxPercentage = 20.0F;
+        float total;
+        
+        ivaValue = amount * taxPercentage / 100;
+        total = amount + ivaValue;
+        
+        return total;
+    }
+
+    public static UsTax getInstace() {
+        return instace;
+    }
+
+    public static void setInstace(UsTax instace) {
+        UsTax.instace = instace;
+    }
+
+    public float ComputeSalesTotal(float value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
+
+       
+   
---
+++ workshops/asmalk/unit3/WS29Crud2.0/Application/src/utils/MongoDB.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+/**
+ *
+ * @author Asmal Kevin
+ */
+public class MongoDB {
+    private String cluster;
+    private MongoClientURI uri;
+    private MongoClient mongoClient;
+    private MongoDatabase database; 
+    private MongoCollection collection;
+
+    public Connection() {
+        cluster = "cloud.mongodb.com/v2/6100e02f62cc7776fd9f9040#metrics/replicaSet/61127b36457892036a7a19a2/explorer/Aircraft/Aircraft/find";
+        uri = new MongoClientURI(cluster);
+        mongoClient = new MongoClient(uri);
+        database= mongoClient.getDatabase("Vehiculos");
+        collection = database.getCollection("Model");
+    }
+    
+}
